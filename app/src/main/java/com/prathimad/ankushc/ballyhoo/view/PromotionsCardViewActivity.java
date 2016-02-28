aec1909b97872eae02cf784fd2b03977485ff694
Ankush049 <choudhary.ankush27@gmail.com>
2016-02-28 19:52:08 +0530

    added menu option on top to launch navigation drawer

--- app/src/main/java/com/prathimad/ankushc/ballyhoo/view/PromotionsCardViewActivity.java
+++ app/src/main/java/com/prathimad/ankushc/ballyhoo/view/PromotionsCardViewActivity.java
@@ -104,14 +104,14 @@ public class PromotionsCardViewActivity extends MenuNavigationActivity implement
         mRecyclerView.addOnItemTouchListener(swipeTouchListener);
 
 
-        /*DrawerLayout drawer = (DrawerLayout) findViewById(R.id.promotion_filter_drawer_layout);
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.promotion_filter_drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
                 this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
         drawer.setDrawerListener(toggle);
         toggle.syncState();
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.promotion_filter_nav_view);
-        navigationView.setNavigationItemSelectedListener(this);*/
+        navigationView.setNavigationItemSelectedListener(this);
 
     }
 
@@ -120,10 +120,9 @@ public class PromotionsCardViewActivity extends MenuNavigationActivity implement
         this.startActivity(intent);
     }
 
-    /*
     @Override
     public void onBackPressed() {
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.promotion_filter_drawer_layout);
         if (drawer.isDrawerOpen(GravityCompat.START)) {
             drawer.closeDrawer(GravityCompat.START);
         } else {
@@ -146,7 +145,7 @@ public class PromotionsCardViewActivity extends MenuNavigationActivity implement
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
+        if (id == R.id.promo_action_settings) {
             return true;
         }
 
@@ -159,22 +158,22 @@ public class PromotionsCardViewActivity extends MenuNavigationActivity implement
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
+        if (id == R.id.promo_nav_camera) {
             // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
+        } else if (id == R.id.promo_nav_gallery) {
 
-        } else if (id == R.id.nav_slideshow) {
+        } else if (id == R.id.promo_nav_slideshow) {
 
-        } else if (id == R.id.nav_manage) {
+        } else if (id == R.id.promo_nav_manage) {
 
-        } else if (id == R.id.nav_share) {
+        } else if (id == R.id.promo_nav_share) {
 
-        } else if (id == R.id.nav_send) {
+        } else if (id == R.id.promo_nav_send) {
 
         }
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
-    }*/
+    }
 }
